61d82baac7bd2d0aa01699faca48b603c6882e1b
Tobias Hintze <thintze+git@tpip.net>
2016-09-01 15:12:58 +0200

    wip

--- src/lib/cdecode.c
+++ src/lib/cdecode.c
@@ -5,6 +5,8 @@ This is part of the libb64 project, and has been placed in the public domain.
 For details, see http://sourceforge.net/projects/libb64
 */
 
+#include <stdio.h>
+
 #include "../../include/cdecode.h"
 
 int base64_decode_value(char value_in) {
@@ -81,5 +83,6 @@ int base64_decode_block(const char *code_in, const int length_in,
     }
   }
   /* control should not reach here */
+fprintf(stderr, "outch\n");
   return plainchar - plaintext_out;
 }
