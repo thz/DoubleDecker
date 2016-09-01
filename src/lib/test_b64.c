f55ff83340b7296049b77d22940a2fa5db9137c7
Tobias Hintze <thintze+git@tpip.net>
2016-09-01 15:01:39 +0200

    wip

---
+++ src/lib/test_b64.c
@@ -0,0 +1,17 @@
+int main(int argc, char **argv)
+{
+
+	const char binput[] = "ab6aa2K5uhY+kBcodeoJ6/Ny+b8tuXhztYxCDbpQx8Q=";
+
+	base64_decodestate state_in;
+
+	char *buf = calloc(1, 33);
+
+	base64_init_decodestate(&state_in);
+
+	int retval = base64_decode_block(binput, sizeof(binput),
+			buf, &state_in);
+
+	return 0;
+}
+
